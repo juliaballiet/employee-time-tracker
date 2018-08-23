d945ae10975ad7e82c1d09f8638d6cbd4716a522
Julia Balliet <julia.m.balliet@gmail.com>
2018-08-23 10:09:23 -0500

    set up routes for views

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,7 +1,19 @@
-let timeApp = angular.module('timeApp', []);
+let timeApp = angular.module('timeApp', [ngRoute]);
 
-timeApp.controller('TimeController', function($http){
-    let vm = this;
-
-    vm.thing = 'words'
+timeApp.config(function($routeProvider){
+    $routeProvider.when('/', {
+        templateUrl: 'views/clockin.html',
+        controller: 'ClockinController as cc'
+    }).when('/employees', {
+        templateUrl: 'views/employees.html',
+        controller: 'EmployeeController as ac'
+    }).when('/timeclock', {
+        templateUrl: 'views/timeclock.html',
+        controller: 'TimeclockController as tc'
+    }).when('/hours', {
+        templateUrl: 'views/total_hours.html',
+        controller: 'HoursController as hc'
+    }).otherwise({
+        template: '<h1>404 page not found</h1>'
+    })
 })
