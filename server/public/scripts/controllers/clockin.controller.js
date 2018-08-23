22cdb939bc6e2340006ecaeea9ee9f2dbbb6cdc6
Julia Balliet <julia.m.balliet@gmail.com>
2018-08-23 12:08:32 -0500

    GET route to show employees clock in status

--- server/public/scripts/controllers/clockin.controller.js
+++ server/public/scripts/controllers/clockin.controller.js
@@ -1,4 +1,21 @@
 timeApp.controller('ClockinController', function($http){
     let vm = this;
-    vm.title = 'Clock In';
+    vm.employeeArray = [];
+
+    getEmployees();
+
+    function getEmployees(){
+        console.log('in getEmployees');
+
+        $http({
+            method: 'GET',
+            url: '/employees'
+        }).then(function(response){
+            console.log('back from server with: ', response.data);
+            vm.employeeArray = response.data;
+        }).catch(function(error){
+            console.log('error: ', error);
+            alert('there was an error getting the employees');
+        })
+    }
 })
