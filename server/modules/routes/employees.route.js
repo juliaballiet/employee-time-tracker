3cd09ffcce9c2e5f70e9a2e1ed688d4fb9ca853c
Julia Balliet <julia.m.balliet@gmail.com>
2018-08-23 12:35:36 -0500

    added PUT route to clock in and out

--- server/modules/routes/employees.route.js
+++ server/modules/routes/employees.route.js
@@ -6,8 +6,20 @@ const pool = require('../pool');
 // routes
 router.get('/', (req, res) => {
     console.log('/employees GET route hit');
-    const query = `SELECT * FROM "employees";`;
-    pool.query(query).then((results) => {
+    const queryText = `SELECT * FROM "employees";`;
+    pool.query(queryText).then((results) => {
+        res.send(results.rows);
+    }).catch((error) => {
+        console.log('error: ', error);
+        res.sendStatus(500);
+    })
+})
+
+router.get('/:code', (req, res) => {
+    console.log('/code GET route hit');
+    clockInCode = req.params.code;
+    const query = `SELECT * FROM "employees" WHERE "clockin_code" = $1;`;
+    pool.query(query, [clockInCode]).then((results) => {
         res.send(results.rows);
     }).catch((error) => {
         console.log('error: ', error);
@@ -18,9 +30,9 @@ router.get('/', (req, res) => {
 router.post('/', (req, res) => {
     console.log('/employees POST route hit with: ', req.body);
     const newEmployee = req.body;
-    const query = `INSERT INTO "employees" ("first_name", "last_name", "clockin_code")
+    const queryText = `INSERT INTO "employees" ("first_name", "last_name", "clockin_code")
                     VALUES ($1, $2, $3);`;
-    pool.query(query, [newEmployee.first_name, newEmployee.last_name, newEmployee.clockin_code])
+    pool.query(queryText, [newEmployee.first_name, newEmployee.last_name, newEmployee.clockin_code])
     .then((results) => {
         res.sendStatus(201);
     }).catch((error) => {
@@ -33,9 +45,21 @@ router.put('/status/:id/:active', (req, res) => {
     console.log('/status PUT hit with: ', req.params.id, req.params.active);
     const employeeToChangeId = req.params.id;
     const employeeToChangeStatus = req.params.active;
-    const query = `UPDATE "employees" SET "active" = $1
+    const queryText = `UPDATE "employees" SET "active" = $1
                     WHERE "id" = $2;`;
-    pool.query(query, [employeeToChangeStatus, employeeToChangeId]).then((results) => {
+    pool.query(queryText, [employeeToChangeStatus, employeeToChangeId]).then((results) => {
+        res.sendStatus(201);
+    }).catch((error) => {
+        console.log('error: ', error);
+        res.sendStatus(500);
+    })
+})
+
+router.put('/:id', (req, res) => {
+    console.log('/id PUT route hit with: ', req.params.id);
+    const clockInId = req.params.id;
+    const queryText = `UPDATE "employees" SET "clocked_in" = NOT "clocked_in" WHERE "id" = $1;`;
+    pool.query(queryText, [clockInId]).then((results) => {
         res.sendStatus(201);
     }).catch((error) => {
         console.log('error: ', error);
@@ -46,12 +70,12 @@ router.put('/status/:id/:active', (req, res) => {
 router.put('/', (req, res) => {
     console.log('/employees PUT hit with: ', req.body);
     const editedEmployee = req.body;
-    const query = `UPDATE "employees" 
+    const queryText = `UPDATE "employees" 
                     SET "first_name" = $1,
                     "last_name" = $2,
                     "clockin_code" = $3
                     WHERE "id" = $4;`;
-    pool.query(query, [editedEmployee.first_name, editedEmployee.last_name, 
+    pool.query(queryText, [editedEmployee.first_name, editedEmployee.last_name, 
                         editedEmployee.clockin_code, editedEmployee.id])
     .then((results) => {
         res.sendStatus(201);
--- server/public/scripts/controllers/clockin.controller.js
+++ server/public/scripts/controllers/clockin.controller.js
@@ -1,21 +1,44 @@
 timeApp.controller('ClockinController', function($http){
     let vm = this;
-    vm.employeeArray = [];
+    vm.clockingIn = false;
 
-    getEmployees();
-
-    function getEmployees(){
-        console.log('in getEmployees');
+    vm.clockInEmployee = function(code){
+        console.log('in clockInEmployee with: ', code);
 
         $http({
             method: 'GET',
-            url: '/employees'
+            url: '/employees/' + code
         }).then(function(response){
             console.log('back from server with: ', response.data);
-            vm.employeeArray = response.data;
+            vm.clockingIn = true;
+            vm.employee = response.data[0];
+            if (vm.employee.clocked_in === false) {
+                vm.status = 'You are currently clocked out.';
+                vm.button = 'clock in'
+            } else {
+                vm.status = 'You are currently clocked in.';
+                vm.button = 'clock out'
+            }
+            vm.clockInCode = '';
         }).catch(function(error){
             console.log('error: ', error);
-            alert('there was an error getting the employees');
+            alert('please enter a valid clockin number!');
+        })
+    }
+
+    vm.changeStatus = function(id){
+        console.log('in changeStatus with: ', id);
+
+        $http({
+            method: 'PUT',
+            url: '/employees/' + id
+        }).then(function(response){
+            console.log('back from server with: ', response);
+            alert('success!');
+            vm.clockingIn = false;
+        }).catch(function(error){
+            console.log('error: ', error);
+            alert('there was an error clocking in');
         })
     }
 })
